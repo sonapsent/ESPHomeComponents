071b4eb6e054605610231cae92470ae3768f1644
Zac Kilburn <kilburn.zachary@gmail.com>
2023-11-28 00:34:11 -0500

    Update __init__.py

---
+++ components/tas5806/__init__.py
@@ -0,0 +1,28 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@bozzobrain"]
+DEPENDENCIES = ["i2c"]
+MULTI_CONF = True
+
+tas5806_ns = cg.esphome_ns.namespace("tas5806")
+TAS5806 = tas5806_ns.class_("TAS5806", cg.Component, i2c.I2CDevice)
+CONF_TAS5806_ID = "tas5806_id"
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(TAS5806),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x2D))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
