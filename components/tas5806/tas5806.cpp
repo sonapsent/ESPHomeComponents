181bc2b521becfb3689ba7882df7de62d23d4374
Zac Kilburn <kilburn.zachary@gmail.com>
2023-11-27 23:32:08 -0500

    Initial commit

---
+++ components/tas5806/tas5806.cpp
@@ -0,0 +1,73 @@
+#include "tas5806.h"
+
+#ifdef USE_ESP32
+
+#include <driver/i2c.h>
+
+#include "esphome/core/application.h"
+#include "esphome/core/hal.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace tas5806 {
+
+static const uint8_t TAS5806_ADDRESS = 0x2D;
+static const uint8_t TAS5806_CTRL_REG = 0x03;
+
+static const uint8_t TAS5806_CTRL_REG_HIZ = 0x02;
+static const uint8_t TAS5806_CTRL_REG_PLAY = 0x03;
+
+static const uint8_t TAS5806_VOL_REG = 0x4C;
+
+static const uint8_t TAS5806_DEVICE_CTRL_1_REG = 0x02;
+
+static const uint8_t DEVICE_CTRL_1_PBTL = (1) << 2;
+
+
+static const uint8_t[2] TAS5806_CONFIGURE_HIZ = {TAS5806_CTRL_REG,TAS5806_CTRL_REG_HIZ};
+static const uint8_t[2] TAS5806_CONFIGURE_PLAY = {TAS5806_CTRL_REG,TAS5806_CTRL_REG_PLAY};
+static const uint8_t[2] TAS5806_CONFIGURE_PBTL = {TAS5806_DEVICE_CTRL_1_REG,DEVICE_CTRL_1_PBTL};
+
+static const char *const TAG = "tas5806";
+
+void MCP4728Component::dump_config() {
+  ESP_LOGCONFIG(TAG, "TAS5806:");
+  LOG_I2C_DEVICE(this);
+  if (this->is_failed()) {
+    ESP_LOGE(TAG, "Communication with TAS5806 failed!");
+  }
+}
+
+
+
+void TAS5806::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up TAS5806 (0x%02X)...", this->address_);
+  auto err = this->write(nullptr, 0);
+  if (err != i2c::ERROR_OK) {
+    this->mark_failed();
+    return;
+  }
+
+// Set HiZ
+  if (this->write(TAS5806_CONFIGURE_HIZ, 2) != i2c::ERROR_OK) {
+    this->status_set_warning();
+    return;
+  }
+  // Set PBTL
+  if (this->write(TAS5806_CONFIGURE_PBTL, 2) != i2c::ERROR_OK) {
+    this->status_set_warning();
+    return;
+  }
+  // Set PLAY
+  if (this->write(TAS5806_CONFIGURE_PLAY, 2) != i2c::ERROR_OK) {
+    this->status_set_warning();
+    return;
+  }
+}
+
+
+
+}  // namespace tas5806
+}  // namespace esphome
+
+#endif  // USE_ESP32
---
+++ components/tas5806/tas5806.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifdef USE_ESP32
+
+
+#include <driver/i2c.h>
+
+#include "esphome/core/component.h"
+#include "esphome/components/i2c/i2c.h"
+
+namespace esphome {
+namespace tas5806 {
+
+
+class TAS5806 : public Component, public i2c::I2CDevice {
+ public:
+  float get_setup_priority() const override { return setup_priority::HARDWARE; }
+
+  void dump_config() override;
+  void setup() override;
+
+ protected:
+
+
+
+};
+
+}  // namespace tas5806
+}  // namespace esphome
+
+#endif  // USE_ESP32
