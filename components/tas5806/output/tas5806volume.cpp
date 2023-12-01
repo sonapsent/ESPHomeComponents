30987af4cff20b4c87d749fe0764a89b8e04a53d
Zac Kilburn <kilburn.zachary@gmail.com>
2023-12-01 13:50:33 -0500

    Finished tie to parent from child

--- components/tas5806/output/tas5806volume.cpp
+++ components/tas5806/output/tas5806volume.cpp
@@ -10,6 +10,7 @@ namespace tas5806 {
 static const char *const TAG = "tas5806volume";
 
 void TAS5806Volume::write_state(float state) {
+  parent_->setVolume(state);
   this->volume_ = state;
     ESP_LOGCONFIG(TAG, "Volume set to %f", state);
 }
--- components/tas5806/output/tas5806volume.h
+++ components/tas5806/output/tas5806volume.h
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include "../tas5806.h"
 
 #include "esphome/core/component.h"
 #include "esphome/components/output/float_output.h"
@@ -11,16 +12,16 @@ namespace tas5806 {
 
 class TAS5806Volume : public Component, public output::FloatOutput {
  public:
-  TAS5806Volume()  {}
+  TAS5806Volume(TAS5806 *parent)  {parent_ = parent}
   
   
   const optional<float> &get_volume() const { return volume_; }
  protected:
   void write_state(float state) override;
+  TAS5806 *parent_;
  private: 
   float volume_ = 50.0f;
   bool update_ = false;
-
 
 };
 
