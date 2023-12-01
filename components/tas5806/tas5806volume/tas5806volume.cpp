4348d2bf42db66eec9f4df9eb14864098de0b259
Zac Kilburn <kilburn.zachary@gmail.com>
2023-12-01 13:22:17 -0500

    Added output class

---
+++ components/tas5806/tas5806volume/tas5806volume.cpp
@@ -0,0 +1,18 @@
+#include "tas5806volume.h"
+
+#include "esphome/core/application.h"
+#include "esphome/core/hal.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace tas5806volume {
+
+static const char *const TAG = "tas5806volume";
+
+void TAS5806Volume::write_state(float state) {
+  this->volume_ = state;
+    ESP_LOGCONFIG(TAG, "Volume set to %f", state);
+}
+
+}  // namespace tas5806
+}  // namespace esphome
---
+++ components/tas5806/tas5806volume/tas5806volume.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+
+#include "esphome/core/component.h"
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/output/float_output.h"
+
+namespace esphome {
+namespace tas5806volume {
+
+
+class TAS5806Volume : public Component, public i2c::I2CDevice, public output::FloatOutput {
+ public:
+  TAS5806Volume()  {}
+  
+  
+  const optional<float> &get_volume() const { return volume_; }
+ protected:
+  void write_state(float state) override;
+ private: 
+  float volume_ = 50.0f;
+  bool update_ = false;
+
+
+};
+
+}  // namespace tas5806volume
+}  // namespace esphome
